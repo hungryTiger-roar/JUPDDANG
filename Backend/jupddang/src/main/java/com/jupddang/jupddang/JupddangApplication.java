52a17e3115d0e6f675f97ebb031aa5aaf07631fa
김진우 <technical1500@gmail.com>
2026-01-20 15:56:18 +0900

    feat: 스프링 프로젝트 생성

---
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/JupddangApplication.java
@@ -0,0 +1,13 @@
+package com.jupddang.jupddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupddangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JupddangApplication.class, args);
+    }
+
+}
---
+++ Backend/jupddang/src/test/java/com/jupddang/jupddang/JupddangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupddang.jupddang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupddangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

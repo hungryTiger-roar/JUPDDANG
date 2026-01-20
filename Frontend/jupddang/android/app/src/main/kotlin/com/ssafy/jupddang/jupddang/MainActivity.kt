b9f9255f98ebf1324646fdefe19b1d0810f1ecd4
DESKTOP-6OO0P6F\SSAFY <elenakim1224@gmail.com>
2026-01-21 07:38:31 +0900

    Fix 플러터 프론트 빈 프로젝트 재생성함 Flutter SDK 3.24.4- > 3.38.x (최신 stable) Android Studio Narwhal JDK 17 -> 기본 JDK JetBrains Runtime 21 (JBR 21) Gradle 8.14 (wrapper) 시작 상태 빈 Flutter 앱

---
+++ Frontend/jupddang/android/app/src/main/kotlin/com/ssafy/jupddang/jupddang/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ssafy.jupddang.jupddang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- Frontend/project/android/app/src/main/kotlin/com/ssafy/zupddang/project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ssafy.zupddang.project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()

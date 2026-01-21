69062b1833def83f99cc81193918a4b447ec1418
민지 <gksals520@naver.com>
2026-01-21 12:27:35 +0900

    [S14P11D208-47] feat: Post, Comment Entity 구현

---
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/sns/entity/Comment.java
@@ -0,0 +1,36 @@
+package com.jupddang.jupddang.sns.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    // 어떤 게시글의 댓글인지 연결
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post postId;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/sns/entity/Post.java
@@ -0,0 +1,50 @@
+package com.jupddang.jupddang.sns.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class) // 자동으로 날짜 기록
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    private String pic; // 기록 사진 임의로 String 설정
+    @Column(name = "like") // db 예약어인 경우 백틱으로 감싸야함
+    @Builder.Default
+    private int like = 0;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // 게시글 하나에 달린 댓글들 (양방향 매핑)
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
+
+    public void increateLike(){
+        this.like++;
+    }
+
+}

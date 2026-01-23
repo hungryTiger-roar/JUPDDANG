43a4f0f2fdb18c739c0dcadb79cc28163cc42a73
배재유 <akakehcn@gmail.com>
2026-01-23 15:09:01 +0900

    fix:refactoring crush

---
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.jupddang.jupddang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 1. 소켓 연결 엔드포인트: ws://localhost:8080/ws
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*"); // CORS 허용
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 2. 메시지 구독 요청 (받는 곳) Prefix: /sub
+        registry.enableSimpleBroker("/sub");
+
+        // 3. 메시지 발행 요청 (보내는 곳) Prefix: /pub
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}

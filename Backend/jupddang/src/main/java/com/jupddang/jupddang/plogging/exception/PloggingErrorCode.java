b4f3dda3b90e5e542119f43cb0745d24d12d8b5c
배재유 <akakehcn@gmail.com>
2026-01-26 16:19:47 +0900

    Edit PloggingErrorCode.java

--- Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/exception/PloggingErrorCode.java
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/exception/PloggingErrorCode.java
@@ -18,6 +18,8 @@ public enum PloggingErrorCode {
     // 공통
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다.");
+
     private final HttpStatus httpStatus;
     private final String message;
 }

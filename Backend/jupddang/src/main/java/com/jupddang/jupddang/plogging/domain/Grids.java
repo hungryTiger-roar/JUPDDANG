2c84a52f0cd8161580a953b4860d1d8ef4295c78
배재유 <akakehcn@gmail.com>
2026-01-26 16:23:42 +0900

    Edit Grids.java

--- Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/domain/Grids.java
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/domain/Grids.java
@@ -21,7 +21,7 @@ public class Grids {
     private String id;
 
     @Column(name = "user_id", nullable = false)
-    private Long userId;
+    private String userId;
 
     @Column(name = "party_id")
     private Long partyId;

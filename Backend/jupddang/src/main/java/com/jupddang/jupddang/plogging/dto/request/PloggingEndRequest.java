9d07d7ceb7ff44c4b688b827f925c2b3d26f1533
김강연 <dio0517@naver.com>
2026-01-29 14:01:31 +0900

    [S14P11D208-221] feat: party 기능 수정 #done

--- Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/dto/request/PloggingEndRequest.java
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/dto/request/PloggingEndRequest.java
@@ -1,5 +1,6 @@
 package com.jupddang.jupddang.plogging.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -9,7 +10,9 @@ public record PloggingEndRequest(
         Double distance,     // 이동 거리
         List<String> LineString, // 이동 경로 좌표
         List<String> trashImages, // 쓰레기 사진 URL
+        Integer times,            // 소요 시간 (변수명 times 통일)
+        Integer score,            // 획득 점수
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        private LocalDateTime endTime;
+        LocalDateTime endTime
 ) {}

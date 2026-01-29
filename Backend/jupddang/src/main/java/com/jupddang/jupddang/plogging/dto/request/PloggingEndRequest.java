eb696a7b49cf4997b6d108a14ca49e46a251a581
배재유 <akakehcn@gmail.com>
2026-01-29 16:17:50 +0900

    Edit PloggingEndRequest.java

--- Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/dto/request/PloggingEndRequest.java
+++ Backend/jupddang/src/main/java/com/jupddang/jupddang/plogging/dto/request/PloggingEndRequest.java
@@ -8,7 +8,6 @@ public record PloggingEndRequest(
         Long ploggingId,
         String content,
         Double distance,     // 이동 거리
-        Integer endTime,            // 소요 시간 (변수명 times 통일)
         Integer score,            // 획득 점수
 
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
